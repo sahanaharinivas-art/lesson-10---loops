08e110c76fcfdabb823a76ca6114d905212e8f6f
harinivassahana-max <harinivassahana@gmail.com>
2025-11-07 16:22:13 +1100

    activity 2

---
+++ activity 2.py
@@ -0,0 +1,9 @@
+#input a word or sentence
+string = input("please enter your own string:")
+
+string2 = (' ')
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
+print("\nThe original string = ", string)
+print("the reversed string = ", string2)
