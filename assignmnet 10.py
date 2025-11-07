44e61a9cca7cf557286a90fccafa11f9acd4fc05
harinivassahana-max <harinivassahana@gmail.com>
2025-11-07 16:45:38 +1100

    assignment 10

---
+++ assignmnet 10.py
@@ -0,0 +1,12 @@
+#power calculator
+
+#get input from user
+base = float(input("enter the base number:"))
+exponent = int(input("enter the exponent (power):"))
+
+#calculate power
+result = base ** exponent
+
+#display result
+print(f"{base} raised to the power of {exponent} is {result}")
+
